e27f271880f8fe35fd8a788aaa20ddef0af175b9
henryhoangduong <henryhoangduong@gmail.com>
2025-04-18 22:10:17 +0700

    abstract_factory

---
+++ Builder/abstract_factory/cruise_motorbike.go
@@ -0,0 +1,13 @@
+package abstractfactory
+
+type CruiseMotorbike struct{}
+
+func (c *CruiseMotorbike) GetWheels() int {
+	return 2
+}
+func (c *CruiseMotorbike) GetSeats() int {
+	return 2
+}
+func (c *CruiseMotorbike) GetType() int {
+	return CruiseMotorbikeType
+}
---
+++ Builder/prototype.go
@@ -0,0 +1,17 @@
+package builder
+
+type ShirtCloner interface {
+	GetClone(m int) (ItemInfoGetter, error)
+}
+
+const (
+	White = 1
+	Black = 2
+	Blue  = 3
+)
+
+func GetShirtsCloner() ShirtCloner {
+	return nil
+}
+
+type ShirtsCache struct{}
