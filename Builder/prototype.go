4ad30838081c6c11ae113f1ff589ecece72ec316
henryhoangduong <henryhoangduong@gmail.com>
2025-04-22 11:21:51 +0700

    main.go

--- Builder/prototype.go
+++ Builder/prototype.go
@@ -1,5 +1,7 @@
 package builder
 
+import "fmt"
+
 type ShirtCloner interface {
 	GetClone(m int) (ItemInfoGetter, error)
 }
@@ -18,4 +20,39 @@ type ShirtsCache struct{}
 
 type ItemInfoGetter interface {
 	GetInfo() string
+}
+
+type ShirtColor byte
+
+type Shirt struct {
+	Price float32
+	SKU   string
+	Color ShirtColor
+}
+
+func (s *Shirt) GetInfo() string {
+	return fmt.Sprintf("Shirt with SKU '%s' and Color id %d that costs %f\n", s.SKU, s.Color, s.Price)
+
+}
+
+var whitPrototype *Shirt = &Shirt{
+	Price: 15.00,
+	SKU:   "empty",
+	Color: White,
+}
+
+var blackPrototype *Shirt = &Shirt{
+	Price: 16.00,
+	SKU:   "empty",
+	Color: Black,
+}
+
+var bluePrototype *Shirt = &Shirt{
+	Price: 17.00,
+	SKU:   "empty",
+	Color: Blue,
+}
+
+func (i *Shirt) GetPrice() float32 {
+	return i.Price
 }
